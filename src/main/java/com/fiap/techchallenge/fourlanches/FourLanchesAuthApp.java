213d12aab8822f71f3ca36977799d027c8b6db01
Lucas Zingano <lucasbz@gmail.com>
2023-11-03 09:48:51 -0300

    improvement: Move classes para pacotes distintos

--- src/main/java/com/fiap/techchallenge/fourlanches/FourLanchesAuthApp.java
+++ src/main/java/com/fiap/techchallenge/fourlanches/FourLanchesAuthApp.java
@@ -1,19 +1,13 @@
-package com.fiap.techchallenge.fourlanches.functions;
+package com.fiap.techchallenge.fourlanches;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import java.util.function.Function;
 
 @SpringBootApplication
 @EnableJpaRepositories
 public class FourLanchesAuthApp {
-    @Bean
-    public Function<String, String> echo() {
-        return payload -> payload;
-    }
+
     public static void main(String[] args) {
         SpringApplication.run(FourLanchesAuthApp.class, args);
     }
