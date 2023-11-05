e43639d8655d4fac1677a6dc574b71f66c5dbb99
Lucas Zingano <lucasbz@gmail.com>
2023-11-05 11:25:44 -0300

    feat: Adiciona geração de token JWT

--- src/test/java/com/fiap/techchallenge/fourlanches/functions/AuthTriggerTest.java
+++ src/test/java/com/fiap/techchallenge/fourlanches/functions/AuthTriggerTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.stubbing.Answer;
 
@@ -25,72 +24,94 @@ class AuthTriggerTest {
 
     @Mock
     private CustomerRepository customerRepository;
+    private ExecutionContext context;
 
     @InjectMocks
     private AuthTrigger function;
 
     @BeforeEach
     public void setup() {
-        this.function = new AuthTrigger(customerRepository);
-    }
+        this.function = new AuthTrigger("fourLanchesTest" ,"123456781234567812345678" , customerRepository);
 
+        this.context = mock(ExecutionContext.class);
+        doReturn(Logger.getGlobal()).when(context).getLogger();
+    }
 
     @Test
     public void shouldReturnOkWhenCPFIsPassed() {
-        // Setup
         @SuppressWarnings("unchecked")
         final HttpRequestMessage<Optional<String>> req = mock(HttpRequestMessage.class);
 
-        final Map<String, String> queryParams = new HashMap<>();
-        queryParams.put("cpf", "00011122233");
-        doReturn(queryParams).when(req).getQueryParameters();
+        doReturn(Map.of("cpf", "00011122233")).when(req).getQueryParameters();
 
+        stubCreateResponseBuilder(req);
+
+        when(this.customerRepository.findByDocument("00011122233")).thenReturn(Optional.of(getExpectedCustomer()));
+
+        final HttpResponseMessage ret = function.run(req, context);
+
+        assertEquals(ret.getStatus(), HttpStatus.OK);
+    }
+
+    @Test
+    public void shouldReturnNotFoundWhenThereAreNoCustomersWithGivenCPF() {
+        @SuppressWarnings("unchecked")
+        final HttpRequestMessage<Optional<String>> req = mock(HttpRequestMessage.class);
+
+        doReturn(Map.of("cpf", "00011122233")).when(req).getQueryParameters();
+
+        stubCreateResponseBuilder(req);
+
+        when(this.customerRepository.findByDocument(any())).thenReturn(Optional.empty());
+
+        final HttpResponseMessage ret = function.run(req, context);
+
+        assertEquals(HttpStatus.NOT_FOUND, ret.getStatus());
+    }
+
+    @Test
+    public void shouldReturnAnonymousTokenWhenCPFIsEmpty() {
+        @SuppressWarnings("unchecked")
+        final HttpRequestMessage<Optional<String>> req = mock(HttpRequestMessage.class);
+
+        stubCreateResponseBuilder(req);
+
+        final HttpResponseMessage ret = function.run(req, context);
+
+        assertEquals(HttpStatus.OK, ret.getStatus());
+    }
+
+    @Test
+    public void shouldReturnInternalServerErrorWhenThereIsAGenericError() {
+        @SuppressWarnings("unchecked")
+        final HttpRequestMessage<Optional<String>> req = mock(HttpRequestMessage.class);
+
+        doReturn(Map.of("cpf", "00011122233")).when(req).getQueryParameters();
+
+        stubCreateResponseBuilder(req);
+
+        when(this.customerRepository.findByDocument(null)).thenThrow(new RuntimeException());
+
+        final HttpResponseMessage ret = function.run(req, context);
+
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, ret.getStatus());
+    }
+
+    private static void stubCreateResponseBuilder(HttpRequestMessage<Optional<String>> req) {
         doAnswer((Answer<HttpResponseMessage.Builder>) invocation -> {
             HttpStatus status = (HttpStatus) invocation.getArguments()[0];
             return new HttpResponseMessageMock.HttpResponseMessageBuilderMock().status(status);
         }).when(req).createResponseBuilder(any(HttpStatus.class));
+    }
 
-        final ExecutionContext context = mock(ExecutionContext.class);
-        doReturn(Logger.getGlobal()).when(context).getLogger();
-
+    private static Customer getExpectedCustomer() {
         var expectedCustomer = new Customer();
         expectedCustomer.setId(1L);
         expectedCustomer.setFirstName("John");
         expectedCustomer.setLastName("Doe");
         expectedCustomer.setEmail("john.doe@email.com");
         expectedCustomer.setDocument("00011122233");
-
-        when(this.customerRepository.findByDocument("00011122233"))
-                .thenReturn(Optional.of(expectedCustomer));
-
-        // Invoke
-        final HttpResponseMessage ret = function.run(req, context);
-
-        // Verify
-        assertEquals(ret.getStatus(), HttpStatus.OK);
+        return expectedCustomer;
     }
 
-    @Test
-    public void shouldReturnBadRequestWhenCPFIsEmpty() {
-        // Setup
-        @SuppressWarnings("unchecked")
-        final HttpRequestMessage<Optional<String>> req = mock(HttpRequestMessage.class);
-
-        doAnswer((Answer<HttpResponseMessage.Builder>) invocation -> {
-            HttpStatus status = (HttpStatus) invocation.getArguments()[0];
-            return new HttpResponseMessageMock.HttpResponseMessageBuilderMock().status(status);
-        }).when(req).createResponseBuilder(any(HttpStatus.class));
-
-        final ExecutionContext context = mock(ExecutionContext.class);
-        doReturn(Logger.getGlobal()).when(context).getLogger();
-
-        when(this.customerRepository.findByDocument(null))
-                .thenReturn(Optional.empty());
-
-        // Invoke
-        final HttpResponseMessage ret = function.run(req, context);
-
-        // Verify
-        assertEquals(ret.getStatus(), HttpStatus.BAD_REQUEST);
-    }
 }
